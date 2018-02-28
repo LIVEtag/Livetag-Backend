0966ec5841f134bd242099f7a5570dbb70e723b9
Andrii Hurzhii <gurzhiy.an@gbksoft.com>
2018-02-28 23:13:46 +0200

    task#0  gitlab ci cd

--- compose-deploy.sh
+++ compose-deploy.sh
@@ -8,12 +8,8 @@ composer config -g github-oauth.github.com $GITHUB_KEY
 composer install --no-interaction --optimize-autoloader
 
 echo "Run init"
-php init
+php init --env=$YII_BUILD_ENV --overwrite=All
 
 # Run database migrations.
 echo "Run migration"
 php ./yii migrate --interactive=0
-
-# Run NPM.
-echo "Run NPM"
-cd design && npm i && npm run build
