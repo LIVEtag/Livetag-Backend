27911deb64512dd33908189781f834f941d4f435
nefedov-da <nefedov.da@gbksoft.com>
2018-04-04 17:04:38 +0300

    task#351: add root CA cert

--- dev/docker/apache2/php7.1/etc/cmd/entrypoint.sh
+++ dev/docker/apache2/php7.1/etc/cmd/entrypoint.sh
@@ -15,13 +15,23 @@ test -f /var/www/html/docker-compose.yml && \
 # Enable SSL
 test -f /var/www/html/.env && . /var/www/html/.env
 test -z "${MAIN_DOMAIN}" || test -f /etc/apache2/ssl/ssl.key || \
-    openssl req -newkey rsa:2048 -nodes -keyout /etc/apache2/ssl/ssl.key -x509 -days 3650 -out /etc/apache2/ssl/ssl.crt -batch \
-    -subj "/C=UA/ST=UA/L=ZP/O=GBKSOFT/OU=IT/CN=${MAIN_DOMAIN}"
+ {
+  echo "authorityKeyIdentifier=keyid,issuer\nbasicConstraints=CA:FALSE\nkeyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment\nsubjectAltName = @alt_names\n\n" > /tmp/v3.ext
+  echo "[alt_names]\nDNS.1 = ${MAIN_DOMAIN}\nDNS.2 = *.${MAIN_DOMAIN}\n" >> /tmp/v3.ext
+  openssl genrsa -out /etc/apache2/ssl/TRUST.key 2048
+  openssl req -x509 -new -nodes -key /etc/apache2/ssl/TRUST.key -sha256 -days 3650 -subj "/C=UA/ST=ZP/L=ZP/O=GBKSOFT/CN=gbksoft.loc" -out /etc/apache2/ssl/TRUST.crt
+  openssl genrsa -out /etc/apache2/ssl/ssl.key 2048
+  openssl req -new -newkey rsa:2048 -sha256 -nodes -key /etc/apache2/ssl/ssl.key -subj "/C=UA/ST=ZP/L=ZP/O=GBKSOFT/CN=${MAIN_DOMAIN}" -out /etc/apache2/ssl/ssl.csr
+  openssl x509 -req -in /etc/apache2/ssl/ssl.csr -CA /etc/apache2/ssl/TRUST.crt -CAkey /etc/apache2/ssl/TRUST.key -CAcreateserial -out /etc/apache2/ssl/ssl.crt -days 3650 -sha256 -extfile /tmp/v3.ext
+  openssl verify -CAfile /etc/apache2/ssl/TRUST.crt /etc/apache2/ssl/ssl.crt || { echo "SSL certificate fail"; exit; }
+ }
+sed -ri "s/#SSLCACertificateFile \/etc\/apache2\/ssl.crt\/ca-bundle.crt/SSLCACertificateFile \/etc\/apache2\/ssl\/TRUST.crt/" /etc/apache2/sites-available/default-ssl.conf
+echo "ServerName ${MAIN_DOMAIN}" >> /etc/apache2/conf-enabled/options.conf
 test -f /etc/apache2/ssl/ssl.key && a2ensite default-ssl
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- apache2-foreground "$@"
+    set -- apache2-foreground "$@"
 fi
 
 exec "$@"
