7a322c4a447b125aaee6e8f0c2d55cd31ca821cb
Sergey Zvonaryov <zvonaryov.si@gbksoft.com>
2018-12-27 11:14:14 +0200

    task#51: update to camel case
    - add faker to fixtures

--- backend/tests/_data/login_data.php
+++ backend/tests/_data/login_data.php
@@ -1,14 +1,16 @@
 <?php
+$faker = Faker\Factory::create();
+
 return [
     [
         'username' => 'erau',
-        'authKey' => 'tUu1qHcde0diwUol3xeI-18MuHkkprQI',
+        'authKey' => \Yii::$app->getSecurity()->generateRandomString(),
         // password_0
         'passwordHash' => '$2y$13$nJ1WDlBaGcbCdbNC5.5l4.sgy.OMEKCqtDQOdQ2OWpgiKRWYyzzne',
         'passwordResetToken' => 'RkD_Jw0_8HEedzLk7MM-ZKEFfYR7VbMr_1392559490',
-        'createdAt' => '1392559490',
-        'updatedAt' => '1392559490',
+        'createdAt' => $faker->unixTime,
+        'updatedAt' => $faker->unixTime,
         'role' => 'advanced',
-        'email' => 'sfriesen@jenkins.info',
+        'email' => $faker->email,
     ],
 ];
--- frontend/tests/_data/user.php
+++ frontend/tests/_data/user.php
@@ -1,24 +1,25 @@
 <?php
+$faker = Faker\Factory::create();
 
 return [
     [
         'username' => 'okirlin',
-        'authKey' => 'iwTNae9t34OmnK6l4vT4IeaTk-YWI2Rv',
+        'authKey' => \Yii::$app->getSecurity()->generateRandomString(),
         'passwordHash' => '$2y$13$CXT0Rkle1EMJ/c1l5bylL.EylfmQ39O5JlHJVFpNn618OUS1HwaIi',
         'passwordResetToken' => 't5GU9NwpuGYSfb7FEZMAxqtuz2PkEvv_' . time(),
         'role' => 'basic',
-        'createdAt' => '1391885313',
-        'updatedAt' => '1391885313',
-        'email' => 'brady.renner@rutherford.com',
+        'createdAt' => $faker->unixTime,
+        'updatedAt' => $faker->unixTime,
+        'email' => $faker->email,
     ],
     [
         'username' => 'troy.becker',
-        'authKey' => 'EdKfXrx88weFMV0vIxuTMWKgfK2tS3Lp',
+        'authKey' => \Yii::$app->getSecurity()->generateRandomString(),
         'passwordHash' => '$2y$13$g5nv41Px7VBqhS3hVsVN2.MKfgT3jFdkXEsMC4rQJLfaMa7VaJqL2',
         'passwordResetToken' => '4BSNyiZNAuxjs5Mty990c47sVrgllIi_' . time(),
         'role' => 'basic',
-        'createdAt' => '1391885313',
-        'updatedAt' => '1391885313',
+        'createdAt' => $faker->unixTime,
+        'updatedAt' => $faker->unixTime,
         'email' => 'nicolas.dianna@hotmail.com',
         'status' => '0',
     ],
