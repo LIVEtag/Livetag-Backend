77f6f3f27ee15d59faf5b1e2e8c70640560e1834
Sergey Zvonaryov <zvonaryov.si@gbksoft.com>
2018-12-27 11:17:54 +0200

    task#51: update to camel case
    - add faker to fixtures

--- common/tests/_data/user.php
+++ common/tests/_data/user.php
@@ -1,15 +1,16 @@
 <?php
 
+$faker = Faker\Factory::create();
 return [
     [
         'username' => 'bayer.hudson',
-        'authKey' => 'HP187Mvq7Mmm3CTU80dLkGmni_FUH_lR',
+        'authKey' => \Yii::$app->getSecurity()->generateRandomString(),
         //password_0
         'passwordHash' => '$2y$13$EjaPFBnZOQsHdGuHI.xvhuDp1fHpo8hKRSk6yshqa9c5EG8s3C3lO',
         'passwordResetToken' => 'ExzkCOaYc1L8IOBs4wdTGGbgNiG3Wz1I_1402312317',
         'role' => 'basic',
-        'createdAt' => '1402312317',
-        'updatedAt' => '1402312317',
-        'email' => 'nicole.paucek@schultz.info',
+        'createdAt' => $faker->unixTime,
+        'updatedAt' => $faker->unixTime,
+        'email' => $faker->email,
     ],
 ];
