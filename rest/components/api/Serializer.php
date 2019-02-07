64c0bd444c037a981cb706365c646f0693c0747d
Alexander Kireev <kireev.ov@gbksoft.com>
2019-02-07 18:35:09 +0200

    task#84: Update serialisation format

--- rest/components/api/Serializer.php
+++ rest/components/api/Serializer.php
@@ -8,6 +8,9 @@ namespace rest\components\api;
 use yii\rest\Serializer as BaseSerializer;
 use rest\components\validation\ErrorMessage;
 use rest\components\validation\ErrorListInterface;
+use yii\base\Arrayable;
+use yii\base\Model;
+use yii\data\DataProviderInterface;
 
 /**
  * Class Serializer
@@ -17,26 +20,34 @@ class Serializer extends BaseSerializer
     /**
      * @inheritdoc
      */
-    public $collectionEnvelope = 'items';
+    public $collectionEnvelope = 'result';
+
+    /**
+     * @var bool Add pagination headers to response
+     */
+    public $addPaginationHeaders = false;
 
     /**
      * @inheritdoc
      */
     public function serialize($data)
     {
-        $data = parent::serialize($data);
-
-        $dataResult = [
-            'code' => $this->response->getStatusCode(),
-            'status' => $this->response->getIsSuccessful() ? 'success' : 'error',
-            'result' => $data,
-        ];
-
-        if (is_array($data) && isset($data[$this->collectionEnvelope])) {
-            $dataResult['result'] = $data[$this->collectionEnvelope];
+        if ($data instanceof Model && $data->hasErrors()) {
+            $serializedData = $this->serializeModelErrors($data);
+        } elseif ($data instanceof Arrayable) {
+            $serializedData = $this->serializeModel($data);
+        } elseif ($data instanceof DataProviderInterface) {
+            $serializedData = $this->serializeDataProvider($data);
+        } else {
+            $serializedData = ['result' => $data];
         }
-
-        return $dataResult;
+        return array_merge(
+            [
+                'code' => $this->response->getStatusCode(),
+                'status' => $this->response->getIsSuccessful() ? 'success' : 'error',
+            ],
+            $serializedData
+        );
     }
 
     /**
@@ -68,6 +79,47 @@ class Serializer extends BaseSerializer
                 'params' => $serializedParams
             ];
         }
+        return ['result' => $result];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function serializeDataProvider($dataProvider)
+    {
+        if ($this->preserveKeys) {
+            $models = $dataProvider->getModels();
+        } else {
+            $models = array_values($dataProvider->getModels());
+        }
+        $models = $this->serializeModels($models);
+        $pagination = $dataProvider->getPagination();
+
+        if ($pagination !== false && $this->addPaginationHeaders) {
+            $this->addPaginationHeaders($pagination);
+        }
+
+        if ($this->request->getIsHead()) {
+            return null;
+        } elseif (!$this->collectionEnvelope) {
+            return $models;
+        }
+
+        $result = [
+            $this->collectionEnvelope => $models,
+        ];
+        if ($pagination !== false) {
+            $serializedPagination = $this->serializePagination($pagination);
+            $result[$this->metaEnvelope]['pagination'] = $serializedPagination[$this->metaEnvelope];
+        }
         return $result;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function serializeModel($model)
+    {
+        return ['result' => parent::serializeModel($model)];
     }
 }
