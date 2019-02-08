ac53ebbd044a0d2fbd986b96bef173b1988bb447
Sergey Zvonaryov <zvonaryov.si@gbksoft.com>
2019-02-08 09:50:54 +0200

    task#89: update packages
    - update packages
    - update perPage

--- rest/components/api/Controller.php
+++ rest/components/api/Controller.php
@@ -38,7 +38,7 @@ class Controller extends BaseController
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     'Access-Control-Request-Headers' => ['*'],
                     'Access-Control-Max-Age' => 86400,
-                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Allow-Headers' => ['Content-Type', 'Authorization'],
                 ],
             ],
--- rest/modules/chat/controllers/ActiveController.php
+++ rest/modules/chat/controllers/ActiveController.php
@@ -74,7 +74,7 @@ class ActiveController extends BaseActiveController
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     'Access-Control-Request-Headers' => ['*'],
                     'Access-Control-Max-Age' => 86400,
-                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Allow-Headers' => ['Content-Type', 'Authorization'],
                 ],
             ],
