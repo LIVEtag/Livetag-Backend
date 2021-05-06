2992e0d8342118dd0b23363a10bfbffbc55b30cf
Anna Darvinska <darvinska.as@gbksoft.com>
2021-05-06 18:27:30 +0300

    SKY2-921. Added resetting likes and views for archived stream session

--- common/observers/StreamSession/DeleteStreamSessionArchiveObserver.php
+++ common/observers/StreamSession/DeleteStreamSessionArchiveObserver.php
@@ -8,8 +8,11 @@ declare(strict_types=1);
 namespace common\observers\StreamSession;
 
 use common\helpers\LogHelper;
+use common\models\Analytics\StreamSessionEvent;
+use common\models\Analytics\StreamSessionStatistic;
 use common\models\Stream\StreamSession;
 use common\models\Stream\StreamSessionArchive;
+use common\models\Stream\StreamSessionLike;
 use RuntimeException;
 use yii\base\Event;
 
@@ -27,16 +30,38 @@ class DeleteStreamSessionArchiveObserver
             throw new RuntimeException('Not StreamSessionArchive instance');
         }
 
-        if ($streamSessionArchive->streamSession->isArchived()) {
-            $streamSessionArchive->streamSession->status = StreamSession::STATUS_STOPPED;
+        $streamSession = $streamSessionArchive->streamSession;
+        if ($streamSession->isArchived()) {
+            $streamSession->status = StreamSession::STATUS_STOPPED;
+            if (!$streamSession->save()) {
+                LogHelper::error(
+                    'Failed to save Stream Session',
+                    StreamSession::LOG_CATEGORY,
+                    LogHelper::extraForModelError($streamSession)
+                );
+            }
         }
 
-        if (!$streamSessionArchive->streamSession->save()) {
-            LogHelper::error(
-                'Failed to save Stream Session',
-                StreamSession::LOG_CATEGORY,
-                LogHelper::extraForModelError($streamSessionArchive->streamSession)
-            );
+        // Remove archived views
+        $streamSessionStatistic = $streamSession->streamSessionStatistic;
+        if ($streamSessionStatistic) {
+            $deleteEventsQuery = StreamSessionEvent::getArchivedEventsQuery($streamSession);
+            /** @var StreamSessionEvent $archivedEvent */
+            foreach ($deleteEventsQuery->each() as $archivedEvent) {
+                $archivedEvent->delete();
+            }
+            StreamSessionStatistic::recalculate($streamSession->getId(), StreamSessionStatistic::ATTR_ARCHIVE_VIEWS_COUNT);
+            StreamSessionStatistic::recalculate($streamSession->getId(), StreamSessionStatistic::ATTR_TOTAL_VIEWS_COUNT);
+        }
+        // Remove archived likes
+        $deleteLikeQuery = StreamSessionLike::find()
+            ->byStreamSessionId($streamSession->getId());
+        if ($streamSession->getStoppedAt()) {
+            $deleteLikeQuery->afterTimestamp($streamSession->getStoppedAt());
+        }
+        /** @var StreamSessionArchive $archivedLike */
+        foreach ($deleteLikeQuery->each() as $archivedLike) {
+            $archivedLike->delete();
         }
     }
 }
