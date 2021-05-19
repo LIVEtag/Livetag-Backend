fe78d09e4c3571f4e33f2f13925b967514877516
Anna Darvinska <darvinska.as@gbksoft.com>
2021-05-19 16:28:02 +0300

    SKY2-1011. Added column for link to parent comment

--- backend/views/stream-session/comment-index.php
+++ backend/views/stream-session/comment-index.php
@@ -46,6 +46,22 @@ use kartik\grid\ActionColumn;
             'hAlign' => GridView::ALIGN_LEFT,
         ],
         [
+            'attribute' => 'parentCommentId',
+            'label' => 'ID of the replied comment',
+            'format' => 'raw',
+            'value' => function (Comment $model) use ($commentSearchModel) {
+                if ($model->parentCommentId) {
+                    return Html::a($model->parentCommentId, [
+                            'view',
+                            'id' => $model->streamSessionId,
+                            'CommentSearch[id]' => $model->parentCommentId
+                        ]
+                    );
+                }
+                return null;
+            }
+        ],
+        [
             'attribute' => 'createdAt',
             'format' => 'datetime',
             'label' => 'Date&Time',
